bb535b3a85ecabec37afc75c25bd5753623424ad
Roman Ulyanenko <ulyanenkorr@gmail.com>
2023-05-01 12:53:42 +0300

    add cryptoApp

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
@@ -20,15 +24,17 @@ class CryptoViewModel : ViewModel() {
 
     private fun loadData() {
         viewModelScope.launch {
-            while (true) {
-                val currentState = _state.value
-                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                    _state.value = State.Loading
+            repository.getCurrencyList()
+                .onStart {
+                    val currentState = _state.value
+                    if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
+                        _state.value = State.Loading
+                    }
                 }
-                val currencyList = repository.getCurrencyList()
-                _state.value = State.Content(currencyList = currencyList)
-                delay(3000)
-            }
+                .filter { it.isNotEmpty() }
+                .onEach { _state.value = State.Content(currencyList = it) }
+                .collect()
+
         }
     }
 }
