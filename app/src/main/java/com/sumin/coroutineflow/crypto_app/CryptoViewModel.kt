b3d45cb5fbf8006392d73439fb0f0790b652156e
Roman Ulyanenko <ulyanenkorr@gmail.com>
2023-05-02 09:32:32 +0300

    change flow in activity

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,40 +1,54 @@
 package com.sumin.coroutineflow.crypto_app
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.util.Log
+import androidx.lifecycle.*
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-        viewModelScope.launch {
-            repository.getCurrencyList()
-                .onStart {
-                    val currentState = _state.value
-                    if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                        _state.value = State.Loading
-                    }
-                }
-                .filter { it.isNotEmpty() }
-                .onEach { _state.value = State.Content(currencyList = it) }
-                .collect()
-
+    val state = repository.getCurrencyList()
+        .filter {
+            Log.d("tag", "filter")
+            it.isNotEmpty()
         }
-    }
+        .map {
+            Log.d("tag", "map")
+            State.Content(currencyList = it) as State
+        }
+        .onStart {
+            Log.d("tag", "start")
+            emit(State.Loading)
+        }
+
+
+//    private fun loadData() {
+//        viewModelScope.launch {
+//            repository.getCurrencyList()
+//                .filter {
+//                    Log.d("tag", "filter" )
+//                    it.isNotEmpty()
+//
+//                }
+//                .map {
+//                    Log.d("tag", "map" )
+//                    State.Content(currencyList = it) as State
+//                }
+//                .onStart {
+//                    Log.d("tag", "start" )
+//                    emit(State.Loading)
+//                }
+//                .onEach {
+//                    _state.value = it
+//                }
+//                .collect()
+//
+//        }
+//    }
+
 }
+
+
